9b745e2c5cbfde0c043b33bfd4770fb6d7c01373
dkssyddico <dkssyddico@naver.com>
2021-12-04 22:42:36 +0900

    order styling

--- src/Pages/Order.js
+++ src/Pages/Order.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Context } from '../context/MyContext';
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
 
 const Section = styled.section`
   padding: 8rem 10rem 5rem;
   height: 100%;
   display: flex;
   flex-direction: column;
-  text-align: center;
   @media screen and (max-width: 1023px) {
     padding: 8rem 4rem 5rem;
   }
@@ -22,10 +23,63 @@ const Title = styled.h1`
   border-bottom: 3px solid black;
   margin-bottom: 2rem;
   font-weight: 500;
+  text-align: center;
 `;
 
 const Container = styled.div`
-  padding: 1.5rem 0;
+  padding: 1rem 0;
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+`;
+
+const Date = styled.h2`
+  color: ${(props) => props.theme.darkGray};
+  font-weight: 500;
+  font-size: 18px;
+  margin-bottom: 1rem;
+`;
+
+const MetaContainer = styled.div`
+  border: 1px solid ${(props) => props.theme.lightGray};
+  padding: 1.2rem 1.5rem;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${(props) => props.theme.lightGray};
+  h3:first-child {
+    font-weight: 500;
+    font-size: 18px;
+  }
+`;
+
+const InfoContainer = styled.div`
+  padding-top: 1rem;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const InfoBox = styled.div`
+  padding: 0.8rem;
+  h4 {
+    font-size: 15px;
+    margin-bottom: 0.8rem;
+  }
+`;
+
+const Span = styled.span`
+  font-weight: 500;
+  font-size: 16px;
 `;
 
 function Order() {
@@ -40,38 +94,44 @@ function Order() {
             let { orderDate } = order;
             let firstItem = order.items[0];
             return (
-              <div>
-                <h2>{`${orderDate}`}</h2>
-                <div>
-                  <div>
-                    <h3>
-                      {order.items.length > 1
-                        ? `${firstItem.name} and ${
-                            order.items.length > 2
-                              ? `${order.items.length - 1} more items`
-                              : `${order.items.length - 1} item`
-                          }`
-                        : `${firstItem.name}`}
-                    </h3>
-                    <h3>Go to Detail</h3>
-                  </div>
-                  <div></div>
-                  <div>
+              <Card key={uuidv4()}>
+                <Date>{`${orderDate}`}</Date>
+                <MetaContainer>
+                  <Link to={`/order/${order.orderId}`}>
+                    <TitleContainer>
+                      <h3>
+                        {order.items.length > 1
+                          ? `${firstItem.name} and ${
+                              order.items.length > 2
+                                ? `${order.items.length - 1} more items`
+                                : `${order.items.length - 1} item`
+                            }`
+                          : `${firstItem.name}`}
+                      </h3>
+                      <h3>Go to Detail</h3>
+                    </TitleContainer>
+                  </Link>
+                  <InfoContainer>
                     <div>
-                      <img width='50px' src={`/images/${firstItem.image}`} alt='product' />
+                      <Image src={`/images/${firstItem.image}`} alt='product' />
                     </div>
-                    <div>
+                    <InfoBox>
                       <h4>
-                        Order No. <span>{order.orderId}</span>
+                        Order No. <Span>{order.orderId}</Span>
                       </h4>
                       <h4>
-                        Total ${' '}
-                        {order.items.reduce((prev, curr) => prev + curr.quantity * curr.price, 0)}
+                        Total{' '}
+                        <Span>
+                          {`$ ${order.items.reduce(
+                            (prev, curr) => prev + curr.quantity * curr.price,
+                            0
+                          )}`}
+                        </Span>
                       </h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                    </InfoBox>
+                  </InfoContainer>
+                </MetaContainer>
+              </Card>
             );
           })}
       </Container>
